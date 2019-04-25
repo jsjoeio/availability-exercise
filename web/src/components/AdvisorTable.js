696e9f02bdb48c7f4dd43d5efe59ef1a9abf272e
Joe Previte <jjprevite@gmail.com>
2019-04-25 12:01:32 -0700

    Remove unused data from AdvisorTable

--- web/src/components/AdvisorTable.js
+++ web/src/components/AdvisorTable.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import AvailableTimeRow from './AvailableTimeRow'
 
-const AdvisorTable = ({ data }) => {
+const AdvisorTable = () => {
   const [advisorAvailability, setAdvisorAvailability] = useState({})
   useEffect(() => {
     async function fetchAdvisorAvailability () {
