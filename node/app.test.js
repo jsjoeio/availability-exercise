e1d7ee81fc95cfb33e90c11c2ee9d4cc25a0dd76
Joe Previte <jjprevite@gmail.com>
2019-04-24 20:24:27 -0700

    Clean up node test styles

--- node/app.test.js
+++ node/app.test.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const app = require('./app')
 
-describe("today", () => {
-    it("returns today's formatted date", () => {
-        expect(app.today()).toBe(new Date().toLocaleDateString());
-    });
-});
+describe('today', () => {
+  it("returns today's formatted date", () => {
+    expect(app.today()).toBe(new Date().toLocaleDateString())
+  })
+})
