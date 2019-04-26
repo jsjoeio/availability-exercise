1d24e270d71bfbfaed87f104169463e8deace933
Joe Previte <jjprevite@gmail.com>
2019-04-25 19:47:57 -0700

    Add logic to fetch bookings in BookedTable

--- web/src/components/BookedTable.js
+++ web/src/components/BookedTable.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BookedTableRow from './BookedTableRow'
 
-const BookedTable = ({ data }) => (
-  <React.Fragment>
-    <h2>Booked Times</h2>
-    <table className='bookings table'>
-      <thead>
-        <tr>
-          <th>Advisor ID</th>
-          <th>Student Name</th>
-          <th>Date/Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* <BookedTableRow advisorId={12345} studentName='Joe Previte' dateTime='2019-04-04T13:00:00-04:00' /> */}
-      </tbody>
-    </table>
-  </React.Fragment>
-)
-
+const BookedTable = ({ data }) => {
+  const [bookedTimes, setBookedTimes] = useState([])
+  useEffect(() => {
+    async function fetchBookings () {
+      try {
+        if (typeof window !== 'undefined') {
+          const res = await fetch('http://localhost:4433/bookings')
+          const json = await res.json()
+          return json
+        }
+      } catch (e) {
+        console.error("Failed to fetch 'today' data", e)
+      }
+    }
+    fetchBookings().then(data => {
+      setBookedTimes(data)
+    })
+  }, [])
+  return (
+    <React.Fragment>
+      <h2>Booked Times</h2>
+      <table className='bookings table'>
+        <thead>
+          <tr>
+            <th>Advisor ID</th>
+            <th>Student Name</th>
+            <th>Date/Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookedTimes.map(bookedTime => (
+            <BookedTableRow advisorId={bookedTime.advisorId} studentName={bookedTime.studentName} dateTime={bookedTime.dateTime} />
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  )
+}
 export default BookedTable
