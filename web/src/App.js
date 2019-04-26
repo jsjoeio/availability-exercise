9fd670ba6332d971b82d759b641e4b0751486cf6
Joe Previte <jjprevite@gmail.com>
2019-04-25 20:30:21 -0700

    Add notes for next steps

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import AdvisorTable from './components/AdvisorTable'
 import BookedTable from './components/BookedTable'
+import Form from './components/Form'
 
-const App = () => (
-  <div className='App container'>
-    <h1>Book Time with an Advisor</h1>
+const App = () => {
+  /*
 
-    <span id='today'>Today is {new Date().toLocaleDateString()}.</span>
-
-    <form id='name-form' className='col-md-6'>
-      <div className='form-group'>
-        <label htmlFor='name-field'>Your Name</label>
-        <input placeholder='Darrell Silver' type='text' id='name-field' className='form-control' />
-      </div>
-    </form>
-
-    <AdvisorTable />
-    <BookedTable />
-  </div>
-)
+  1. Store name in app...
+  2. figure out how to pass down name
+  3. Pass helper createBooking function to advisorTable
+  4. When you create a booking and receieve a 201, you need to call two functions
+  4a. One function removes it from the AdvisorTable
+  4b. The other function adds it to the BookedTable
+  */
+  return (
+    <div className='App container'>
+      <h1>Book Time with an Advisor</h1>
+      <span id='today'>Today is {new Date().toLocaleDateString()}.</span>
+      <Form />
+      <AdvisorTable />
+      <BookedTable />
+    </div>
+  )
+}
 
 export default App
