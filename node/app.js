3412917a531257e831c0cc8b6a900ec46f2e7da6
Joe Previte <jjprevite@gmail.com>
2019-04-25 19:41:39 -0700

    Add bookings endpoint

--- node/app.js
+++ node/app.js
@@ -5,6 +5,12 @@ const { transformAdvisorData, fetchAdvisorData } = require('./utils/helpers')
 const app = express()
 const port = 4433
 
+const bookingsDb = [{
+  advisorId: 34562,
+  studentName: 'Joe Previte',
+  dateTime: '2019-04-04T13:00:00-04:00'
+}]
+
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -14,6 +20,10 @@ app.get('/advisors', (req, res) => {
       const advisorData = transformAdvisorData(data)
       res.send(advisorData)
     })
+})
+
+app.get('/bookings', (req, res) => {
+  res.send(bookingsDb)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
