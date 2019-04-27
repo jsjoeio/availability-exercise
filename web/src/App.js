e428f0492ef1250c4f32920989140ec11ee266eb
Joe Previte <jjprevite@gmail.com>
2019-04-27 16:27:01 -0700

    Add form validation to check for name before booking

--- web/src/App.js
+++ web/src/App.js
@@ -6,38 +6,42 @@ import { createBooking } from './utils/helpers'
 import uuidv4 from 'uuid/v4'
 
 const App = () => {
+  const [isValid, setIsValid] = useState(true)
+  const [shouldResetName, setShouldResetName] = useState(false)
   let savedName
   function getName (name) {
     savedName = name
   }
   const [optimisticBookings, setOptimisticBookings] = useState([])
   async function bookAppointment (advisorId, dateTime) {
-    try {
-      const newBooking = {
-        bookingId: uuidv4(),
-        advisorId,
-        dateTime,
-        studentName: savedName
+    if (savedName.trim() !== '') {
+      try {
+        const newBooking = {
+          bookingId: uuidv4(),
+          advisorId,
+          dateTime,
+          studentName: savedName
+        }
+        const response = await createBooking(newBooking)
+        // If successfully created on server, save newBooking
+        if (response.status === 201) {
+          setOptimisticBookings([...optimisticBookings, newBooking])
+          setShouldResetName(true)
+          setShouldResetName(false)
+        }
+      } catch (error) {
+        console.error(error)
       }
-      const response = await createBooking(newBooking)
-      // If successfully created on server, save newBooking
-      if (response.status === 201) {
-        setOptimisticBookings([...optimisticBookings, newBooking])
-      }
-    } catch (error) {
-      console.error(error)
+    } else {
+      setIsValid(false)
     }
   }
-  /*
 
-  4. When you create a booking and receieve a 201, you need to call two functions
-  4a. One function removes it from the AdvisorTable
-  */
   return (
     <div className='App container'>
       <h1>Book Time with an Advisor</h1>
       <span id='today'>Today is {new Date().toLocaleDateString()}.</span>
-      <Form getName={getName} />
+      <Form getName={getName} isValid={isValid} shouldResetName={shouldResetName} />
       <AdvisorTable optimisticBookings={optimisticBookings} bookAppointment={bookAppointment} />
       <BookedTable optimisticBookings={optimisticBookings} />
     </div>
--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 
-const Form = ({ getName }) => {
+const Form = ({ getName, isValid, shouldResetName }) => {
   const [name, setName] = useState('')
   getName(name)
+  if (shouldResetName && name !== '') {
+    setName('')
+  }
   return (
-    <form id='name-form' className='col-md-6'>
+    <form style={{ position: 'relative' }} id='name-form' className='col-md-6'>
       <div className='form-group'>
         <label htmlFor='name-field'>Your Name</label>
-        <input placeholder='Darrell Silver' type='text' id='name-field' className='form-control' value={name} onChange={e => {
-          setName(e.target.value)
-        }} />
+        <input placeholder='Darrell Silver' type='text' id='name-field' className={`form-control ${!isValid && 'is-invalid'}`} value={name} onChange={e => setName(e.target.value)} />
+        {!isValid && <div style={{ display: 'inline-block', position: 'absolute' }} className='invalid-feedback'>Please provide your name before booking</div>}
       </div>
     </form>
   )
