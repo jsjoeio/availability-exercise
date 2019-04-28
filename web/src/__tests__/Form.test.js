1c7e340d1b938a39bcd7e966a1627b8e60a11dc7
Joe Previte <jjprevite@gmail.com>
2019-04-28 16:29:38 -0700

    Update Form test

--- web/src/__tests__/Form.test.js
+++ web/src/__tests__/Form.test.js
@@ -5,7 +5,7 @@ import Form from '../components/Form'
 describe('Form component', () => {
   it('should let you type in your name', () => {
     const fakeFn = jest.fn()
-    const { container, getByPlaceholderText, getByDisplayValue } = render(<Form getName={fakeFn} isValid shouldResetName={false} />)
+    const { rerender, getByPlaceholderText, getByDisplayValue } = render(<Form getName={fakeFn} isValid shouldResetName={false} />)
     const input = getByPlaceholderText('Darrell Silver')
     // Simulate user adding name to input
     fireEvent.change(input, { target: { value: 'Dan Friedman' } })
@@ -13,20 +13,20 @@ describe('Form component', () => {
 
     // Update the props, re-render to the same container
     // This fakes our user clicking "Book"
-    render(<Form getName={fakeFn} isValid shouldResetName />, { container })
+    rerender(<Form getName={fakeFn} isValid shouldResetName />)
     // Input value should now be empty
     expect(input.value).toEqual('')
   })
 
   it('should show an error message if isValid is false', () => {
     const fakeFn = jest.fn()
-    const { container, getByPlaceholderText, getByText } = render(<Form getName={fakeFn} isValid shouldResetName={false} />)
+    const { rerender, getByPlaceholderText, getByText } = render(<Form getName={fakeFn} isValid shouldResetName={false} />)
     const input = getByPlaceholderText('Darrell Silver')
     // Input value should be empty to start
     expect(input.value).toEqual('')
     // Update the props, re-render to the same container
     // Pretend as if the user clicked book without filling out name
-    render(<Form getName={fakeFn} isValid={false} shouldResetName={false} />, { container })
+    rerender(<Form getName={fakeFn} isValid={false} shouldResetName={false} />)
     // Input value should now be empty
     expect(getByText('Please provide your name before booking')).toBeTruthy()
   })
